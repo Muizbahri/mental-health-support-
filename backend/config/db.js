434ffb480c3181b379b4c150ca7acb5541b61685
Muiz Bahri <muizbahri2002@gmail.com>
2025-06-29 12:15:10 +0800

    your message

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,7 @@ const db = mysql.createConnection({
   host: process.env.DB_HOST || '127.0.0.1',
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || 'root',
-  database: process.env.DB_NAME || 'mental_health_db',
+  database: process.env.DB_NAME || 'mental_health',
   dateStrings: true
 });
 
--- backend/models/db.js
+++ backend/models/db.js
@@ -4,8 +4,8 @@ require('dotenv').config();
 const pool = mysql.createPool({
   host: process.env.DB_HOST || '127.0.0.1',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'mental_health_db',
+  password: process.env.DB_PASSWORD || 'root',
+  database: process.env.DB_NAME || 'mental_health',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
